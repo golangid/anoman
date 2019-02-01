df74b103353f88ea365615a2437868d48c33895f
wuriyanto <wuriyanto48@yahoo.co.id>
2019-02-01 10:45:07 +0700

    test output parser

--- anoman.go
+++ anoman.go
@@ -1,8 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"os"
+	"strings"
+)
+
+var (
+	questions = []string{
+		"project name ?",
+		"subtitle ?",
+		"author name ?",
+	}
 )
 
 func main() {
@@ -15,5 +26,35 @@ func main() {
 
 	flag.Parse()
 
-	fmt.Println(outputName)
+	reader := bufio.NewReader(os.Stdin)
+
+	var (
+		temp TemplateMD
+
+		responses []string
+	)
+
+	for _, question := range questions {
+
+		fmt.Printf("%s : ", question)
+
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		response = strings.TrimSpace(response)
+
+		responses = append(responses, response)
+	}
+
+	temp.ProjectName = responses[0]
+	temp.Subtitle = responses[1]
+	temp.Author = responses[2]
+
+	if err := ParseOutput(temp, os.Stdin); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
